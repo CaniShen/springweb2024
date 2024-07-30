a9c1803595ec2569d2fb832c80e97cb108b14b71
CaniShen <164502320+CaniShen@users.noreply.github.com>
2024-07-30 13:51:57 +0200

    30-07-2024(clase)

--- 15_escuela_notas/src/main/java/dao/MatriculasDao.java
+++ 15_escuela_notas/src/main/java/dao/MatriculasDao.java
@@ -1,5 +1,7 @@
 package dao;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
@@ -11,4 +13,12 @@ public interface MatriculasDao extends JpaRepository<Matricula, MatriculaPk> {
 	double avgByIdCurso(int idCurso);
 	@Query("select avg(m.nota) from Matricula m where m.alumno.usuario=?1")
 	double avgByUsuario(String usuario);
+	
+	
+	@Query(value="select idCurso, nota from matriculas",nativeQuery = true)
+	List<Object[]> buscarDatos();
+	@Query(value="select alumnos.nombre,alumnos.email, matriculas.nota from alumnos, matriculas, cursos"
+			+ "where alumnos.usuario=matriculas.usuario and matriculas.idCurso=cursos.idCurso and cursos=?", nativeQuery=true)
+	List<Object[]> datosAlumnoCurso(int idCurso);
+	
 }
