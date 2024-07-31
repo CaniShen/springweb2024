6e9e5b43d2f423af4a708547f993711929388a22
CaniShen <164502320+CaniShen@users.noreply.github.com>
2024-07-31 13:18:56 +0200

    31-07-2014(clase)

---
+++ 21_banca_boot/src/main/java/model/CuentaDto.java
@@ -0,0 +1,37 @@
+package model;
+
+
+
+public class CuentaDto {
+	private int numeroCuenta;
+	private double saldo;
+	private String tipoCuenta;
+	public CuentaDto(int numeroCuenta, double saldo, String tipoCuenta) {
+		super();
+		this.numeroCuenta = numeroCuenta;
+		this.saldo = saldo;
+		this.tipoCuenta = tipoCuenta;
+	}
+	public CuentaDto() {
+		super();
+	}
+	public int getNumeroCuenta() {
+		return numeroCuenta;
+	}
+	public void setNumeroCuenta(int numeroCuenta) {
+		this.numeroCuenta = numeroCuenta;
+	}
+	public double getSaldo() {
+		return saldo;
+	}
+	public void setSaldo(double saldo) {
+		this.saldo = saldo;
+	}
+	public String getTipoCuenta() {
+		return tipoCuenta;
+	}
+	public void setTipoCuenta(String tipoCuenta) {
+		this.tipoCuenta = tipoCuenta;
+	}
+
+}
---
+++ 21_banca_boot/src/main/java/utilidades/Mapeador.java
@@ -0,0 +1,35 @@
+package utilidades;
+
+import entities.Cliente;
+import entities.Cuenta;
+import entities.Movimiento;
+import model.ClienteDto;
+import model.CuentaDto;
+import model.MovimientoDto;
+
+
+public class Mapeador {
+	public ClienteDto clienteEntityToDto(Cliente cliente) {
+		return new ClienteDto(cliente.getDni(),
+								cliente.getNombre(),
+								cliente.getDireccion(),
+								cliente.getTelefono());
+	}
+	
+	public MovimientoDto movimientoEntityToDto(Movimiento movimiento) {
+		return new MovimientoDto(movimiento.getIdMovimiento(),
+									movimiento.getCuenta().getNumeroCuenta(),
+									movimiento.getFecha(),
+									movimiento.getCantidad(),
+									movimiento.getOperacion());
+	}
+	
+	
+	public CuentaDto cuentaEntityToDto(Cuenta cuenta) {
+		return new CuentaDto(cuenta.getNumeroCuenta(),
+							cuenta.getSaldo(),
+							cuenta.getTipoCuenta());
+							
+	}
+
+}
